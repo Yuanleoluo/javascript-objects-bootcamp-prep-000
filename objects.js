04f03980c4714b26919e7c8fad471966c22bc42b
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-06 02:18:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {yearsnyears: "king", hasley: "americano", macholmore: "samelove"}
 
 function updatePlaylist(playL, artistName, songTitle){
-  playL[artistName] = songTitle
+  playL.artistName = songTitle
   return playL
 }
