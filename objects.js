2e98026f27f1c0c3ffd02f74ff296504e325389e
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-06 02:10:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = {yearsnyears: "king", hasley: "americano", macholmore: "samelove"}
+
+function updatePlaylist(playL, artistName, songTitle){
+  playL.artistName = songTitle
+  return playL
+}
